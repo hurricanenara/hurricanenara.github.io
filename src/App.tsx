4d37b9c9cf26c77ef7e15643de85c905f9011761
ponyolovesham <naralee0124@gmail.com>
2022-03-19 17:49:15 -0400

    add intro banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import css from "./styles.module.css";
 
 function App() {
-  return <div className='App'>hi i'm nara</div>;
+  return (
+    <div className={css.appContainer}>
+      <div className={css.intro}>hi i'm nara</div>
+    </div>
+  );
 }
 
 export default App;
